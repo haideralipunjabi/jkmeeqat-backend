ca204adc93afdd25318824fe65baf47433843b0e
Haider Ali Punjabi <haiderali@cyberservices.com>
2023-11-29 19:10:20 +0530

    Changed hash calculation

--- scripts/generate.py
+++ scripts/generate.py
@@ -20,7 +20,8 @@ def dict_hash(dictionary):
     dhash = hashlib.md5()
     # We need to sort arguments so {'a': 1, 'b': 2} is
     # the same as {'b': 2, 'a': 1}
-    encoded = json.dumps(dictionary, sort_keys=True).encode()
+    encoded = json.dumps(dictionary).encode()
+    print(encoded)
     dhash.update(encoded)
     return dhash.hexdigest()
 
